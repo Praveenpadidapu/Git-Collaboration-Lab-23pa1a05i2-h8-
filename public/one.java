0353332b836b013499c8a292dc5017ebc096393f
Vivek-Nerella <23pa1a05h8@vishnu.edu.in>
2026-02-01 17:13:45 +0530

    pulled pushedd

--- public/one.java
+++ public/one.java
@@ -4,9 +4,7 @@ public class one {
         return a + b;
     }
     
-    public static double subtract(double a, double b) {
-        return a - b;
-    }
+    
     
     public static double multiply(double a, double b) {
         return a * b;
