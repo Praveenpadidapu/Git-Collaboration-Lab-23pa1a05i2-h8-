af872a5fad8b53a534217a4019db2a52eb2f289f
Vivek-Nerella <23pa1a05h8@vishnu.edu.in>
2026-02-01 17:12:52 +0530

    changed file

--- public/one.java
+++ public/one.java
@@ -4,9 +4,7 @@ public class one {
         return a + b;
     }
     
-    public static double subtract(double a, double b) {
-        return a - b;
-    }
+    
     
     public static double multiply(double a, double b) {
         return a * b;
@@ -23,7 +21,7 @@ public class one {
     public static void main(String[] args) {
         System.out.println("Calculator");
         System.out.println("Addition: 10 + 5 = " + add(10, 5));
-        System.out.println("Subtraction: 10 - 5 = " + subtract(10, 5));
+        //System.out.println("Subtraction: 10 - 5 = " + subtract(10, 5));
         System.out.println("Multiplication: 10 * 5 = " + multiply(10, 5));
         System.out.println("Division: 10 / 5 = " + divide(10, 5));
     }
